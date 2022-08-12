82cac3c22ad84813e507863daa6c9faf429d4dd8
pedro-lamego <pedrownlamego@hotmail.com>
2022-08-12 19:49:57 +0100

    main fixed

--- podiz/lib/main.dart
+++ podiz/lib/main.dart
@@ -5,8 +5,12 @@ import 'package:flutter/services.dart';
 import 'package:flutter_locales/flutter_locales.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:podiz/aspect/app_router.dart';
+import 'package:podiz/aspect/constants.dart';
 import 'package:podiz/aspect/theme/themeConfig.dart';
+import 'package:podiz/splashScreen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'providers.dart';
 
 late SharedPreferences preferences;
 
@@ -70,36 +74,32 @@ class _MyAppState extends ConsumerState<MyApp> with WidgetsBindingObserver {
 
   @override
   Widget build(BuildContext context) {
-    // final userLoadingValue = ref.watch(userLoadingProvider);
+    final userLoadingValue = ref.watch(userLoadingProvider);
     final goRouter = ref.watch(goRouterProvider);
     print('my app gorouter');
     return LocaleBuilder(
       builder: (locale) => MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        localizationsDelegates: Locales.delegates,
-        supportedLocales: Locales.supportedLocales,
-        locale: locale,
-        routerDelegate: goRouter.routerDelegate,
-        routeInformationParser: goRouter.routeInformationParser,
-        routeInformationProvider: goRouter.routeInformationProvider,
-        restorationScopeId: 'app',
-        theme: ThemeConfig.light,
-        themeMode: ref.watch(themeModeProvider),
-        builder: (context, child) {
-          print('my app builder');
-          // return userLoadingValue.when(
-          // error: (e, _) {
-          //   print('main: ${e.toString()}');
-          //   return SplashScreen.error();
-          // },
-          // loading: () => SplashScreen(),
-          // data: (_) {
-          //   setScreenSize(context);
-          return child!;
-          // },
-          // );
-        },
-      ),
+          debugShowCheckedModeBanner: false,
+          localizationsDelegates: Locales.delegates,
+          supportedLocales: Locales.supportedLocales,
+          locale: locale,
+          routerDelegate: goRouter.routerDelegate,
+          routeInformationParser: goRouter.routeInformationParser,
+          routeInformationProvider: goRouter.routeInformationProvider,
+          restorationScopeId: 'app',
+          theme: ThemeConfig.light,
+          themeMode: ref.watch(themeModeProvider),
+          builder: (context, child) => userLoadingValue.when(
+                error: (e, _) {
+                  print('main: ${e.toString()}');
+                  return SplashScreen.error();
+                },
+                loading: () => SplashScreen(),
+                data: (_) {
+                  setScreenSize(context);
+                  return child!;
+                },
+              )),
     );
   }
 }
