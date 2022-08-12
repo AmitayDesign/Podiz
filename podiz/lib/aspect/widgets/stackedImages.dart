2b88ca83395c79e3e4333287c866cc03aa46af25
ggalambas <ggalambas@gmail.com>
2022-08-12 21:09:02 +0100

    stacked iamges

--- podiz/lib/aspect/widgets/stackedImages.dart
+++ podiz/lib/aspect/widgets/stackedImages.dart
@@ -1,59 +1,38 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
-import 'package:podiz/home/components/podcastAvatar.dart';
+import 'package:podiz/aspect/extensions.dart';
 import 'package:podiz/objects/Podcast.dart';
 
 class StackedImages extends StatelessWidget {
   final Podcast podcast;
-  final double size;
-
-  const StackedImages(this.podcast, {Key? key, required this.size})
+  final double radius;
+  const StackedImages(this.podcast, {Key? key, required this.radius})
       : super(key: key);
+
+  List<String> decideImages(List<String> images) {
+    if (images.length <= 3) return images;
+    return images.sublist(images.length - 4, images.length - 1);
+  }
 
   @override
   Widget build(BuildContext context) {
-    int dif = size ~/ 2;
-    final shift = size - dif;
-    List<String> images = decideList(podcast.commentsImg);
-    List<Widget> widgets = images
-        .asMap()
-        .map((index, img) {
-          final image = Container(
-            width: size,
-            height: size,
-            margin: EdgeInsets.only(left: shift * index),
-            child: buildimages(context, img, size),
-          );
-          return MapEntry(index, image);
-        })
-        .values
-        .toList();
-
-    return widgets.isEmpty
-        ? Container()
-        : Stack(
-            children: widgets,
-          );
-  }
-
-  Widget buildimages(BuildContext context, String path, double size) {
-    return ClipOval(
-      child: Container(
-        color: const Color(0xFF262626),
-        padding: const EdgeInsets.all(1),
-        child: ClipOval(
-            child: PodcastAvatar(
-          imageUrl: path,
-          size: size,
-        )),
-      ),
+    return Stack(
+      children: decideImages(podcast.commentsImg)
+          .mapIndexed(
+            (i, imageUrl) => Container(
+              margin: EdgeInsets.only(left: radius * i),
+              padding: const EdgeInsets.all(2),
+              decoration: BoxDecoration(
+                color: context.colorScheme.surface,
+                shape: BoxShape.circle,
+              ),
+              child: CircleAvatar(
+                radius: radius,
+                backgroundImage: NetworkImage(imageUrl),
+              ),
+            ),
+          )
+          .toList(),
     );
-  }
-
-  decideList(List<String> images) {
-    if (images.length <= 3) {
-      return images;
-    } else {
-      return images.sublist(images.length - 3, images.length - 1);
-    }
   }
 }
