cbec884571b05963baacde819297d5e41c0085fc
agalambas <agalambas@gmail.com>
2022-08-18 09:56:32 +0100

    comments add to firebase fix

--- podiz/lib/src/features/discussion/data/firestore_discussion_repository.dart
+++ podiz/lib/src/features/discussion/data/firestore_discussion_repository.dart
@@ -84,10 +84,9 @@ class FirestoreDiscussionRepository implements DiscussionRepository {
       comment.toJson(),
     );
     // add comment to user comments list
-    batch.update(
-      firestore.collection("users").doc(user.id),
-      comment.toJson(),
-    );
+    batch.update(firestore.collection("users").doc(user.id), {
+      'comments': FieldValue.arrayUnion([comment.toJson()]),
+    });
     // increment podcast comment counter
     //TODO do not save img urls, save user ids
     //! then swap user argument for userId
