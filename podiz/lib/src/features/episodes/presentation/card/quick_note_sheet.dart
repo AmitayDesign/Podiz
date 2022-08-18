a701065257f5abf248db2a21a0daadb23a873668
agalambas <agalambas@gmail.com>
2022-08-18 10:45:47 +0100

    users listening

--- podiz/lib/src/features/episodes/presentation/card/quick_note_sheet.dart
+++ podiz/lib/src/features/episodes/presentation/card/quick_note_sheet.dart
@@ -5,6 +5,7 @@ import 'package:podiz/aspect/extensions.dart';
 import 'package:podiz/src/features/auth/data/auth_repository.dart';
 import 'package:podiz/src/features/discussion/data/discussion_repository.dart';
 import 'package:podiz/src/features/discussion/presentation/comment/comment_text_field.dart';
+import 'package:podiz/src/features/episodes/data/episode_repository.dart';
 import 'package:podiz/src/features/episodes/domain/episode.dart';
 import 'package:podiz/src/features/player/data/player_repository.dart';
 import 'package:podiz/src/theme/palette.dart';
@@ -42,15 +43,44 @@ class QuickNoteSheet extends ConsumerWidget {
             ),
             Padding(
               padding: const EdgeInsets.fromLTRB(4, 8, 4, 4),
-              child: Text(
+              child: UsersListening(
+                episode: episode,
                 //TODO locales text
-                "${episode.userIdsWatching.length} listening right now",
-                style: context.textTheme.bodySmall,
+                textBuilder: (count, _) => "$count listening right now",
               ),
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+//TODO move to another file
+class UsersListening extends ConsumerWidget {
+  final Episode episode;
+  final String Function(
+    int totalUsersWatching,
+    int otherUsersWatching,
+  ) textBuilder;
+
+  const UsersListening({
+    Key? key,
+    required this.episode,
+    required this.textBuilder,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final liveEpisode =
+        ref.watch(episodeStreamProvider(episode.id)).valueOrNull ?? episode;
+    final user = ref.watch(currentUserProvider);
+    final totalUsersWatching = liveEpisode.userIdsWatching;
+    final otherUsersWatching = totalUsersWatching..remove(user);
+    //TODO add faces
+    return Text(
+      textBuilder(totalUsersWatching.length, otherUsersWatching.length),
+      style: context.textTheme.bodySmall,
     );
   }
 }
