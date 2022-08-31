a5f45f30e2bbe221d4a0fa016d49adce4c2ba064
ggalambas <ggalambas@gmail.com>
2022-08-31 15:40:07 +0100

    first 2 steps of walkthrought

--- podiz/lib/src/app.dart
+++ podiz/lib/src/app.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_locales/flutter_locales.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:podiz/src/routing/app_router.dart';
+import 'package:showcaseview/showcaseview.dart';
 
 import 'features/auth/data/auth_repository.dart';
 import 'features/splash/presentation/splash_screen.dart';
 import 'theme/app_theme.dart';
+import 'utils/instances.dart';
 
 class MyApp extends ConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -30,9 +32,15 @@ class MyApp extends ConsumerWidget {
         builder: (context, child) => Consumer(builder: (context, ref, _) {
           final firstUserValue = ref.watch(firstUserFutureProvider);
           return firstUserValue.when(
-            error: (e, _) =>  const SplashScreen.error(),
+            error: (e, _) => const SplashScreen.error(),
             loading: () => const SplashScreen(),
-            data: (_) => child!,
+            data: (_) => ShowCaseWidget(
+              disableAnimation: true,
+              disableBarrierInteraction: true,
+              onFinish: () =>
+                  ref.read(preferencesProvider).setBool('first-time', false),
+              builder: Builder(builder: (context) => child!),
+            ),
           );
         }),
       ),
