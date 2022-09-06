c0d8ef0cb18a4b6095d0050db70fa8e4563d18ff
agalambas <agalambas@gmail.com>
2022-09-06 15:17:01 +0100

    auth

--- podiz/lib/src/features/auth/presentation/onboarding/onboarding_screen.dart
+++ podiz/lib/src/features/auth/presentation/onboarding/onboarding_screen.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_locales/flutter_locales.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
-import 'package:podiz/src/common_widgets/loading_button.dart';
-import 'package:podiz/src/routing/app_router.dart';
 import 'package:podiz/src/theme/context_theme.dart';
 
+import 'budz_page.dart';
 import 'connect_page.dart';
 import 'intro_page.dart';
 import 'onboarding_bar.dart';
-import 'onboarding_controller.dart';
 
 /// The sub-routes that are presented as part of the on boarding page.
-enum OnboardingPage { intro, connect }
+enum OnboardingPage { intro, budz, connect }
+
+extension OnboardingPageX on OnboardingPage {
+  T when<T>({
+    T Function()? intro,
+    T Function()? budz,
+    T Function()? connect,
+  }) {
+    switch (this) {
+      case OnboardingPage.intro:
+        if (intro != null) return intro();
+        throw Exception('Missing intro implementation');
+      case OnboardingPage.budz:
+        if (budz != null) return budz();
+        throw Exception('Missing budz implementation');
+      case OnboardingPage.connect:
+        if (connect != null) return connect();
+        throw Exception('Missing connect implementation');
+    }
+  }
+}
 
 /// This is the root widget of the on boarding page, which is composed of 2 views
 ///
@@ -27,8 +43,6 @@ class OnboardingScreen extends ConsumerStatefulWidget {
 class _OnboardingScreenState extends ConsumerState<OnboardingScreen> {
   final controller = PageController();
   var view = OnboardingPage.intro;
-
-  bool get isStartView => view == OnboardingPage.intro;
 
   @override
   void dispose() {
@@ -48,28 +62,21 @@ class _OnboardingScreenState extends ConsumerState<OnboardingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    ref.listen<AsyncValue>(onboardingControllerProvider, (_, state) {
-      if (!state.isRefreshing && state.hasError) {
-        showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-            title: const Text('Error'),
-            content: Text(state.error.toString()),
-          ),
-        );
-      }
-    });
-    final state = ref.watch(onboardingControllerProvider);
-
     // Return a Scaffold with a PageView containing the views.
     // This allows for a nice scroll animation when switching between pages.
     // Note: only the currently active view will be visible.
     return WillPopScope(
-      onWillPop: () async {
-        if (isStartView) return true;
-        goToView(OnboardingPage.intro);
-        return false;
-      },
+      onWillPop: () async => view.when(
+        intro: () => true,
+        budz: () {
+          goToView(OnboardingPage.intro);
+          return false;
+        },
+        connect: () {
+          goToView(OnboardingPage.budz);
+          return false;
+        },
+      ),
       child: DecoratedBox(
         decoration: BoxDecoration(
           color: context.colorScheme.background,
@@ -81,38 +88,19 @@ class _OnboardingScreenState extends ConsumerState<OnboardingScreen> {
         child: Scaffold(
           appBar: const OnboardingBar(),
           backgroundColor: Colors.transparent,
-          body: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: PageView(
-                    // disable swiping between pages
-                    physics: const NeverScrollableScrollPhysics(),
-                    controller: controller,
-                    children: const [IntroPage(), ConnectPage()],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 4,
-                    vertical: 16,
-                  ),
-                  child: LoadingElevatedButton(
-                    loading: state.isLoading,
-                    onPressed: () => isStartView
-                        ? goToView(OnboardingPage.connect)
-                        : context.pushNamed(AppRoute.signIn.name),
-                    child: Text(
-                      isStartView
-                          ? Locales.string(context, "intro2")
-                          : Locales.string(context, "intro4"),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+          body: PageView(
+            // disable swiping between pages
+            physics: const NeverScrollableScrollPhysics(),
+            controller: controller,
+            children: [
+              IntroPage(
+                onSuccess: () => goToView(OnboardingPage.budz),
+              ),
+              BudzPage(
+                onSuccess: () => goToView(OnboardingPage.connect),
+              ),
+              const ConnectPage(),
+            ],
           ),
         ),
       ),
