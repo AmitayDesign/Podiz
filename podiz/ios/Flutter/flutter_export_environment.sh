54e0e40352138d0efea47e4d4f0f593a8f7d1c90
ggalambas <ggalambas@gmail.com>
2022-09-12 14:15:40 +0100

    button colors

--- podiz/ios/Flutter/flutter_export_environment.sh
+++ podiz/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter\flutter_windows\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\pedro\Documents\Upwork\podiz"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\ggala\OneDrive\Documents\flutter projects\Upwork\podiz"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
