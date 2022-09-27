7c89c10da745bf7dd3b5199141c515fa731bff2f
agalambas <agalambas@gmail.com>
2022-09-27 18:30:49 +0100

    notification navigation to the correct time

--- podiz/lib/src/features/episodes/presentation/home_screen.dart
+++ podiz/lib/src/features/episodes/presentation/home_screen.dart
@@ -122,6 +122,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
             context.pushNamed(
               AppRoute.discussion.name,
               params: {'episodeId': comment.episodeId},
+              queryParams: {'t': comment.timestamp.inSeconds},
             );
             break;
           case Channel.follows:
