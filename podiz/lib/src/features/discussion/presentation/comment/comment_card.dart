6834f84106241f61937491575442fa540461ab13
ggalambas <ggalambas@gmail.com>
2022-11-18 17:41:57 +0000

    unused imports

--- podiz/lib/src/features/discussion/presentation/comment/comment_card.dart
+++ podiz/lib/src/features/discussion/presentation/comment/comment_card.dart
@@ -1,9 +1,6 @@
 import 'dart:io' as io;
-import 'dart:io';
 
 import 'package:device_info_plus/device_info_plus.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
